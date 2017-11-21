719ec11fbfae942286b07529ba17f172beaa6aab
Thaddäus Töppen <t.toeppen@gmx.de>
2017-11-21 12:49:12 +0100

    Run execs independently for eventual timing

--- scripts/run_plot_model.sh
+++ scripts/run_plot_model.sh
@@ -1,12 +1,20 @@
 #! /usr/bin/env bash
 
-echo "Compiling lotka_volterra"
-nim c --verbosity:0 -f -r -d:release ../examples/lotka_volterra.nim
-echo ""
-echo "Compiling rma"
-nim c --verbosity:0 -f -r -d:release ../examples/rma.nim
-echo ""
-echo "Compiling chemostat"
-nim c --verbosity:0 -f -r -d:release ../examples/chemostat.nim
+echo "*** Compiling lotka_volterra"
+nim c --verbosity:0 -f -d:release ../examples/lotka_volterra.nim
+echo "*** Running chemostat"
+../examples/lotka_volterra
 
+echo "*** Compiling rma"
+nim c --verbosity:0 -f -d:release ../examples/rma.nim
+echo "*** Running chemostat"
+../examples/rma
+
+echo "*** Compiling chemostat"
+nim c --verbosity:0 -f -d:release ../examples/chemostat.nim
+echo "*** Running chemostat"
+../examples/chemostat
+
+echo "*** Plotting data"
 python3 plot_data.py
+echo "*** Done"
