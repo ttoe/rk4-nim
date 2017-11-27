3e3126e789dbe86f12a408100262d255f668860c
Thaddäus Töppen <t.toeppen@gmx.de>
2017-11-27 23:29:39 +0100

    Add more minima and maxima helper functions

--- examples/chemostat.nim
+++ examples/chemostat.nim
@@ -84,14 +84,18 @@ while bifVal <= (bifParEnd + 0.5*bifParStep):
 proc maximaIxs(fs: seq[float]): seq[int] =
   var maxIxs = newSeq[int](0)
   for i in 1..(fs.high-1):
-    if fs[i] > fs[i-1] and fs[i] > fs[i+1]:
-      maxIxs.add(i)
-    if fs[i-1] == fs[i] and fs[i] == fs[i+1]:
+    if (fs[i] > fs[i-1] and fs[i] > fs[i+1]) or fs[i-1] == fs[i] and fs[i] == fs[i+1]:
       maxIxs.add(i)
   return maxIxs
 
 proc minimaIxs(fs: seq[float]): seq[int] =
   return maximaIxs(fs.map(x => x * (-1)))
+
+proc maximaVals(fs: seq[float]): seq[float] =
+  return maximaIxs(fs).map(i => fs[i])
+
+proc minimaVals(fs: seq[float]): seq[float] =
+  return minimaIxs(fs).map(i => fs[i])
 
 # proc removeConsecutiveMaxIxsAndKeepMiddleOne
 
@@ -99,7 +103,7 @@ proc minimaIxs(fs: seq[float]): seq[int] =
 let vals: seq[float] = @[0.1,0.2,0.4,0.5,0.2,0.1,-0.1,0.0,-0.2,0.1,0.3]
 
 echo maximaIxs(vals)
-echo maximaIxs(vals).map(i => vals[i])
+echo maximaVals(vals)
 
 echo minimaIxs(vals)
-echo minimaIxs(vals).map(i => vals[i])
+echo minimaVals(vals)
