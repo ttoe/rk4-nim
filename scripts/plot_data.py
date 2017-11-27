0fb9cf0d3471236ae8f06c90fb35335795e56f93
Thaddäus Töppen <t.toeppen@gmx.de>
2017-11-28 00:17:16 +0100

    Only compile and plot what i am working on

--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -1,21 +1,21 @@
 from matplotlib import pylab as plt
 import pandas as pd
 
-# lotka-volterra
-lv_data = pd.read_csv("lv_data.csv", sep="\t", header=0, index_col=False)
-
-lv_data.plot(x="time", y=["X", "Y"])
-plt.xlabel("time")
-plt.ylabel("population density")
-plt.savefig("lotka_volterra.png")
-
-# rma
-rma_data = pd.read_csv("rma_data.csv", sep="\t", header=0, index_col=False)
-
-rma_data.plot(x="time", y=["X", "Y"])
-plt.xlabel("time")
-plt.ylabel("population density")
-plt.savefig("rma.png")
+### # lotka-volterra
+### lv_data = pd.read_csv("lv_data.csv", sep="\t", header=0, index_col=False)
+### 
+### lv_data.plot(x="time", y=["X", "Y"])
+### plt.xlabel("time")
+### plt.ylabel("population density")
+### plt.savefig("lotka_volterra.png")
+### 
+### # rma
+### rma_data = pd.read_csv("rma_data.csv", sep="\t", header=0, index_col=False)
+### 
+### rma_data.plot(x="time", y=["X", "Y"])
+### plt.xlabel("time")
+### plt.ylabel("population density")
+### plt.savefig("rma.png")
 
 # chemostat
 chemostat_data = pd.read_csv("chemostat_data.csv", sep="\t", header=0, index_col=False)
--- scripts/run_plot_model.sh
+++ scripts/run_plot_model.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-echo "*** Compiling lotka_volterra"
-nim c --verbosity:0 -f -d:release ../examples/lotka_volterra.nim
-echo "*** Running chemostat"
-../examples/lotka_volterra
-
-echo "*** Compiling rma"
-nim c --verbosity:0 -f -d:release ../examples/rma.nim
-echo "*** Running chemostat"
-../examples/rma
+### echo "*** Compiling lotka_volterra"
+### nim c --verbosity:0 -f -d:release ../examples/lotka_volterra.nim
+### echo "*** Running chemostat"
+### ../examples/lotka_volterra
+### 
+### echo "*** Compiling rma"
+### nim c --verbosity:0 -f -d:release ../examples/rma.nim
+### echo "*** Running chemostat"
+### ../examples/rma
 
 echo "*** Compiling chemostat"
 nim c --verbosity:0 -f -d:release ../examples/chemostat.nim
