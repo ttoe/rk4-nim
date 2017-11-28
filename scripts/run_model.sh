a9c6bb0f78f703accdf45bbe8709783c3debb76c
Thaddäus Töppen <t.toeppen@gmx.de>
2017-11-28 23:24:34 +0100

    Add script to run mdoel without plotting

---
+++ scripts/run_model.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+### echo "*** Compiling lotka_volterra"
+### nim c --verbosity:0 -f -d:release ../examples/lotka_volterra.nim
+### echo "*** Running chemostat"
+### ../examples/lotka_volterra
+### 
+### echo "*** Compiling rma"
+### nim c --verbosity:0 -f -d:release ../examples/rma.nim
+### echo "*** Running chemostat"
+### ../examples/rma
+
+echo "*** Compiling chemostat"
+nim c --verbosity:0 -f -d:release ../examples/chemostat.nim
+echo "*** Running chemostat"
+../examples/chemostat
